73ba015e15c301fd49d0439163d82e68731fa22c
AmarjitAmar <Amarpreetkaur610@gmail.com>
2018-07-06 18:28:36 +0530

    add Toast Toast

--- app/src/main/java/com/example/dell/githubintegration/MainActivity.java
+++ app/src/main/java/com/example/dell/githubintegration/MainActivity.java
@@ -11,5 +11,6 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         Toast.makeText(this, "hi", Toast.LENGTH_SHORT).show();
+        Toast.makeText(this, "hiii", Toast.LENGTH_SHORT).show();
     }
 }
